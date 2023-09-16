ff7b64eba6e30e791860f467b0fe52263b37a0eb
Gurveer Sohal <gurveersohal433@gmail.com>
2023-09-16 15:39:32 -0600

    printing currently running processes in a table

--- main.go
+++ main.go
@@ -6,10 +6,32 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
+
+type Container struct{
+	mu sync.Mutex
+	Table map[int]string 
+}
+
+func (c *Container) add(name string, pid int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Table[pid] = name
+}
+
+func (c *Container) remove(pid int) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	delete(c.Table, pid)
+}
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+	c := Container{
+		Table: make(map[int]string),
+	}
+
 	for {
 		fmt.Print("goshell % ")
 		text, err := reader.ReadString('\n')
@@ -25,6 +47,14 @@ func main() {
 			if strings.Compare("exit", cmd) == 0 {
 				// exit command
 				break
+			} else if strings.Compare("list", cmd) == 0 {
+				if (len(c.Table) == 0) {
+					fmt.Println("No processes currently running")
+				}
+				fmt.Printf("PID\tName\t\n")
+				for k, v := range c.Table {
+					fmt.Printf("%d\t%s\t\n", k, v)
+				}
 			} else {
 				path, err := exec.LookPath(cmd)
 				if err != nil {
@@ -43,9 +73,21 @@ func main() {
 
 		} else {
 			cmd := cmdList[0]
+			if strings.Compare("kill", cmd) == 0 {
+				
+			}
 			// check for background process
 			if strings.Compare("&", cmdList[len(cmdList)-1]) == 0 {
-				fmt.Println("working on background things")
+				path, err := exec.LookPath(cmd)
+				if err != nil {
+					printError("error when looking for path", err)
+					continue
+				}
+				args := cmdList
+				args[0] = path
+				var procAttr os.ProcAttr
+				procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
+				go runBackgroundProcess(path, args, &procAttr, text, &c)
 			} else {
 				path, err := exec.LookPath(cmd)
 				if err != nil {
@@ -63,6 +105,18 @@ func main() {
 				}
 			}
 		}
+	}
+}
+
+func runBackgroundProcess(path string, args []string, procAttr *os.ProcAttr, name string, c *Container) {
+	if process, err := os.StartProcess(path, args, procAttr); err != nil {
+		printError("error when running your process", err)
+	} else {
+		c.add(name, process.Pid)
+		fmt.Println(c.Table[process.Pid])
+		process.Wait()
+		fmt.Printf("done with process %d\n", process.Pid)
+		c.remove(process.Pid)
 	}
 }
 
